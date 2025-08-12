ef6313008d573534107ff2e5d4cea78ecd96c039
riz <rizrama4@gmail.com>
2025-08-12 11:20:43 +0700

    update hello with paramater name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/RandomNet4/go-hello
+module github.com/RandomNet4/go-hello/v2
 
 go 1.21.0
--- hello.go
+++ hello.go
@@ -1,5 +1,5 @@
 package gohello
 
-func SayHello() string {
-	return "Hello,indonesia merdeka"
+func SayHello(name string) string {
+	return "Hello" + name
 }
